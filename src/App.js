a234350eaf17bfbad7de92bd12dcc64855ffcda5
baloyd <danielbaloy@gmail.com>
2021-07-29 18:28:22 -0400

    pulled folders out of my-app folder;created portfolioContainer.js;need to work on conditional rendering

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Navbar from '../src/components/Navbar.js';
+import Bio from '../src/components/Bio.js'
+import Footer from '../src/components/Footer.js'
+import PortfolioContainer from "../src/components/PortfolioContainer";
+import './styles/App.css';
+
+
+function App() {
+  return (<div>
+    <Navbar />
+    <Bio />
+    <Footer />
+
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import Navbar from './Navbar';
+import Home from './pages/Home';
+import About from './pages/Bio';
+import Contact from './pages/Contact';
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('Home');
+
+  // TODO: Add a comment describing the functionality of this method
+  const renderPage = () => {
+    if (currentPage === 'Home') {
+      return <Home />;
+    }
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    return <Contact />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
+     
+      {renderPage()}
+    </div>
+  );
+}
