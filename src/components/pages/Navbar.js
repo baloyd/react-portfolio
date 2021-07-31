f3ad6995b41b90e3e978b894a39eae3c41db7a27
baloyd <danielbaloy@gmail.com>
2021-07-31 09:34:50 -0400

    conditional rendering working; link styling broken; contact & resume components need rework

--- src/components/pages/Navbar.js
+++ src/components/pages/Navbar.js
@@ -3,16 +3,35 @@ import '../../styles/App.css';
 
 
 
-function Navbar() {
+function Navbar({currentPage, handlePageChange}) {
     return (
         <div>
         <header className="header">
           <h1 className="text-muted">Daniel Baloy</h1>
           <nav id="nav">
-              <a href="#Bio" className="text-muted">Bio</a>
-              <a href="#Portfolio" className="text-muted">Portfolio</a>
-              <a href="#Contact" className="text-muted">Contact</a>
-              <a href="#Resume" className="text-muted">Resume</a>
+              <a href="#Bio"
+               onClick={() => handlePageChange('Bio')}
+                className=
+                {currentPage === 'Bio' ? 'nav-link active' : 'nav-link'} >
+                    Bio</a>
+
+              <a href="#Portfolio"
+              onClick={() => handlePageChange('Portfolio')} 
+              className=
+              {currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}>
+                  Portfolio</a>
+
+              <a href="#Contact"
+               onClick={() => handlePageChange('Contact')}
+                className=
+                {currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}>
+                    Contact</a>
+
+              <a href="#Resume"
+              onClick={() => handlePageChange('Resume')} 
+              className=
+                {currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}>
+                    Resume</a>
           </nav>
       </header>
       <img className="banner" alt=""></img>
