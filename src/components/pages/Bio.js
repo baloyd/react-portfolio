1dc41501ac3cf9b3b79d0225c05ce6e64617a745
baloyd <danielbaloy@gmail.com>
2021-08-01 11:39:10 -0400

    added developer foto;fixed header alignments

--- src/components/pages/Bio.js
+++ src/components/pages/Bio.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import '../../styles/App.css';
-
+import polaroid from '../../images/polaroid-frame.png'
 function Bio() {
     return (
         
       <div>
           <article class="article">
           <h2 id="Bio">Bio</h2>
+          <img alt="" id="polaroid" src={polaroid}></img>
+         
           <p>
                Front-end web developer utilizing a fine art background to create a superior, polished UI. <br></br>
                Recently earned a certificate from University of Miami’s Full Stack Web Development Coding Boot Camp. <br></br>
                Self-motivated, adaptable problem-solver that feels just as at ease taking the lead on a project as in working as part of a team.<br></br>
                 Recently collaborated on creating a full stack app that functions as an online book club.<br></br>
           </p>
-
       </article>
+    
       </div>
 
     )
